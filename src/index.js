3a9fabf45dba7d7e390bfe8269890a7f56bbec61
Yoonjae Yoo <ryunzae@gmail.com>
2019-07-06 10:07:16 +0900

    Update ws access port

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 const Bootstrap = require("./Bootstrap");
 const Server = require("./Server");
 const Client = require("./Client");
+const ip = require('ip');
 
 const server = new Server();
 server.start();
 
 (async () => {
-    const bootstrap = new Bootstrap("http://192.168.35.217:3000");
-    const ips = await bootstrap.fetch();
-    ips.forEach(ip => {
-        const client = new Client("ws://" + ip);
+    const bootstrap = new Bootstrap("http://192.168.35.2");
+    const ipAddrs = await bootstrap.fetch();
+    ipAddrs.filter(addr => addr !== ip.address()).forEach(addr => {
+        const client = new Client("ws://" + addr + ":43210");
         client.connect();
     })
 })();
