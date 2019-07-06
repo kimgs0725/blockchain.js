a05d9e38395e072b4f0ab14c56f5fd01f4221270
Yoonjae Yoo <ryunzae@gmail.com>
2019-07-06 11:30:04 +0900

    update

--- src/Client.js
+++ src/Client.js
@@ -1,9 +1,9 @@
 const WebSocketClient = require("websocket").client;
-const utils = require("./utils");
 const NodeRSA = require("node-rsa");
+const utils = require("./utils");
 
-const PUB_KEY = "";
-const PRIV_KEY = "";
+const PUB_KEY = "303c300d06092a864886f70d0101010500032b003028022100b8be42a7593c3e4d9605476ddfcf73fd95b86901f05af77dc2ea77d823dc01790203010001";
+const PRIV_KEY = "3081aa020100022100b8be42a7593c3e4d9605476ddfcf73fd95b86901f05af77dc2ea77d823dc01790203010001022042b09c0b215178043605cdd54217f77c935200a38443f5e98d9da10c44127769021100f343064c66ab234d9225bd59ee059bbb021100c26ac8158d6a349b7ecbb62a71d7925b021039ed3df4b7860d340d973b0dcd263cbd02105c15799983b6971e434b3788190db2db0211008610a279cd31d66169eba5cffebcfae8";
 
 class Client {
     constructor(hostname) {
