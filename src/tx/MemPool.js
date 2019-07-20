433528e1ac983525a1415aacdb258727671adaf1
Yoonjae Yoo <ryunzae@gmail.com>
2019-07-20 12:26:41 +0900

    Add methods to MemPool

--- src/tx/MemPool.js
+++ src/tx/MemPool.js
@@ -2,6 +2,14 @@ class MemPool {
     constructor() {
         this.txs = {};
     }
+
+    addTx(tx) {
+        this.txs[tx.hash()] = tx;
+    }
+
+    removeTx(txHash) {
+        delete this.txs[txHash];
+    }
 }
 
 module.exports = MemPool;
